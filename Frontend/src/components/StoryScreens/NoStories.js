ded1951ebb5b8aff5dc8ee0ff0aa8aa1fd82dab5
deev-anderson <accanderson.barros@gmail.com>
2024-05-27 21:10:59 -0300

    fix phrase

--- Frontend/src/components/StoryScreens/NoStories.js
+++ Frontend/src/components/StoryScreens/NoStories.js
@@ -3,7 +3,7 @@ import React from "react";
 const NoStories = () => (
   <div className="no-story">
     <h2>Nenhuma postagem encontrada</h2>
-    <p>Tente uma pesquisa uma palavra diferente.</p>
+    <p>Tente uma pesquisar uma palavra diferente.</p>
   </div>
 );
 
